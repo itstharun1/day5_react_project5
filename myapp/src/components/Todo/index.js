d2fdff486705e0c9b0e516ac383652a397bec7f5
itstharun1 <tharunganjai@gmail.com>
2024-03-10 14:52:16 +0530

    day5

---
+++ myapp/src/components/Todo/index.js
@@ -0,0 +1,74 @@
+import { Component } from "react";
+
+import './index.css'
+
+import {v4 as uuidv4} from 'uuid';
+
+import TodoItem from "../TodoItem";
+
+const todoList=[
+    {
+        id:uuidv4(),
+        name: "Buy groceries",
+        completed: false
+    },{
+        id:uuidv4(),
+        name:"Clean the house",
+        completed:false
+    },{
+        id:uuidv4(),
+        name: "Go for a run",
+        completed:false
+    },
+]
+
+class Todo extends Component{
+    state={
+        newTodoList:todoList,
+        inputVal:""
+
+    }
+
+    addinput=(event)=>{
+        this.setState({inputVal:event.target.value})
+    }
+
+    submitInput=()=>{
+        const item={id:uuidv4(),name:this.state.inputVal,completed:false};
+        console.log(item);
+        //add to list
+        let list=this.state.newTodoList;
+        list.push(item)
+        this.setState({
+            newTodoList:list
+        })
+        
+    }
+    onDelete=(id)=>{
+        const {newTodoList}=this.state
+        const newData=newTodoList.filter((each)=>(each.id!==id))
+        this.setState({newTodoList:newData});
+    }
+
+    render(){
+
+        const {newTodoList,inputVal}=this.state
+        console.log(inputVal)
+
+        return(
+            <div className="div2">
+                <h1>Todo List</h1>
+                <div>
+                    <input onChange={this.addinput} type="text" /> <button onClick={this.submitInput}>Add Task</button>
+                </div>
+                <div>
+                    {newTodoList.map((item) => (
+                        <TodoItem onDelete={this.onDelete} key={item.id} item={item}/>  
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Todo
---
+++ myapp/src/components/TodoItem/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+const TodoItem=(props)=>{
+
+    const {item,onDelete}=props
+    const {name,id}=item;
+
+    const deleteThis=()=>{
+        onDelete(id)
+    }
+
+    return(
+        <div className='div1'>
+            <input className='btn2' type="checkbox" />
+            <h1 className='completed1'>{name}</h1>
+            <button onClick={deleteThis} className='btn1'>Delete</button>
+        </div>
+    )
+}
+export default TodoItem
